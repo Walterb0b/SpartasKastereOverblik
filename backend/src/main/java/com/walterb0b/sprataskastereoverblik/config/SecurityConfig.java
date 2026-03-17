e6b3dfbb1d0934e0fb49f8e17951ac030b8023d2
Walterb0b <osca0001@stud.ek.dk>
2026-03-17 19:32:19 +0100

    new pages

--- backend/src/main/java/com/walterb0b/sprataskastereoverblik/config/SecurityConfig.java
+++ backend/src/main/java/com/walterb0b/sprataskastereoverblik/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/import/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .httpBasic(Customizer.withDefaults());
 
